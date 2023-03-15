caf3e8ca1967930e7cdee9a45e06524195a10e73
minchae123 <08minchae17@gmail.com>
2023-03-15 11:01:38 +0900

    0315

---
+++ RayTest/Assets/01_Scripts/RayCaster.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    [SerializeField] private float maxDis = 5;
+    public LayerMask layerMask;
+    private void OnDrawGizmos()
+    {
+        RaycastHit hit;
+
+        /*bool isHit = Physics.Raycast(
+            transform.position, 
+            transform.forward, 
+            out hit, 
+            maxDis, layerMask);*/
+
+        bool isHit = Physics.BoxCast(
+            transform.position,
+            transform.lossyScale * 0.5f,
+            transform.forward,
+            out hit,
+            transform.rotation,
+            maxDis);
+
+        if (isHit)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position, transform.forward * maxDis);
+        }
+    }
+}

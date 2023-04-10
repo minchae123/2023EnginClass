bf96e3343e3cb18c2d4d6168f6c1a293ce60b85a
minchae123 <08minchae17@gmail.com>
2023-04-10 13:00:36 +0900

    0410--

--- 3DClass/Assets/01_Scripts/Enemy/AIStates/AttackAIState.cs
+++ 3DClass/Assets/01_Scripts/Enemy/AIStates/AttackAIState.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackAIState : MonoBehaviour
+public class AttackAIState : CommonAIState
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float rotateSpeed = 720f;
+
+    protected Vector3 targetVector;
+
+    public override void OnEnterState()
+    {
+        enemyController.NavMovement.StopImmediately();
+        enemyController.AgentAnimator.OnAnimationEventTrigger += AttackAnimationEndHandle;
+        enemyController.AgentAnimator.OnAnimationEventTrigger += AttackCollsionHandle;
+        aiActionData.IsAttacking = false;
+    }
+
+    public override void OnExitState()
+    {
+        enemyController.AgentAnimator.OnAnimationEventTrigger -= AttackCollsionHandle;
+        enemyController.AgentAnimator.OnAnimationEventTrigger -= AttackAnimationEndHandle;
+
+        enemyController.AgentAnimator.SetAttackState(false);
+        enemyController.AgentAnimator.SetAttackTrigger(false);
+    }
+
+    private void AttackAnimationEndHandle()
+    {
+        enemyController.AgentAnimator.SetAttackState(false);
+        aiActionData.IsAttacking = false;
+    }
+
+    private void AttackCollsionHandle()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateState()
     {
-        
+        base.UpdateState();
+
+        if(aiActionData.IsAttacking == false)
+        {
+            SetTarget();
+            Quaternion rot = Quaternion.LookRotation(targetVector);
+            transform.rotation = rot;
+
+            aiActionData.IsAttacking = true;
+            enemyController.AgentAnimator.SetAttackState(true);
+            enemyController.AgentAnimator.SetAttackTrigger(true);
+        }
+    }
+
+    private void SetTarget()
+    {
+        targetVector = enemyController.TargetTrm.position - transform.position;
+        targetVector.y = 0;
     }
 }

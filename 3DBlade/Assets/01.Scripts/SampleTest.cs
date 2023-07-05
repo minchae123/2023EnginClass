eaaf58ac928ca52a3141b3fa75db1efff6a07a57
minchae123 <08minchae17@gmail.com>
2023-07-05 10:38:57 +0900

    0705

---
+++ 3DBlade/Assets/01.Scripts/SampleTest.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGGM : IEnumerable
+{
+    private int[] arr = { 1, 2, 3 };
+    private int index = 0;
+
+    public IEnumerator GetEnumerator()
+    {
+        yield return arr[0];
+        yield return arr[1];
+        yield return arr[2];
+    }
+}
+
+public class CoroutineHandle : IEnumerator
+{
+    public bool IsDone { get; private set; }
+    public object Current { get; }
+
+    public bool MoveNext()
+    {
+        return !IsDone;
+    }
+
+    public void Reset()
+    {
+
+    }
+
+    public CoroutineHandle(MonoBehaviour owner, IEnumerator coroutine)
+    {
+        Current = owner.StartCoroutine(Wrap(coroutine));
+    }
+
+    private IEnumerator Wrap(IEnumerator coroutine)
+    {
+        yield return coroutine;
+        IsDone = true;
+    }
+}
+
+public class SampleTest : MonoBehaviour
+{
+    private MyGGM ggm = new MyGGM();
+
+    private IEnumerator Start()
+    {
+        var task1 = StartCoroutine(CoA());
+        var task2 = StartCoroutine(CoB());
+
+        //yield return CoA();
+        //yield return CoB();
+
+        yield return task1; 
+        yield return task2;
+
+        Debug.Log("Job Complete");
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (var a in ggm)
+            {
+                Debug.Log(a);
+            }
+        }
+    }
+
+    private IEnumerator CoA()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("Co A Complete");
+    }
+
+    private IEnumerator CoB()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Co B Complete");
+    }
+}

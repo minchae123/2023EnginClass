92a26ebed1532cf83609040cf5d201f078326175
minchae123 <08minchae17@gmail.com>
2023-07-12 10:32:16 +0900

    0712

--- 3DBlade/Assets/01.Scripts/AddressbleTest.cs
+++ 3DBlade/Assets/01.Scripts/AddressbleTest.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class AddressbleTest : MonoBehaviour
 {
@@ -12,14 +13,32 @@ public class AddressbleTest : MonoBehaviour
         {
             LoadPopupText();
         }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            UnloadPopupText();
+        }
     }
+
+    private AsyncOperationHandle<GameObject> handle;
+    private GameObject popupText;
 
     private void LoadPopupText()
     {
         Addressables.LoadAssetAsync<GameObject>($"PopupText").Completed += obj =>
         {
-            Debug.Log(obj);
-            Debug.Log(obj.Result);
+            handle = obj;
+
+            popupText = Instantiate(handle.Result, Vector3.zero, Quaternion.identity);
+
+            //Debug.Log(obj);
+            //Debug.Log(obj.Result);
         };
+    }
+
+    private void UnloadPopupText()
+    {
+        Destroy(popupText);
+        Addressables.Release(handle); // 핸들을 아예 빼서 메모리에서 빼준다
     }
 }

37fa0034232e86d9a1160545ef568280e804c1bd
minchae123 <08minchae17@gmail.com>
2023-08-17 15:31:05 +0900

    input

---
+++ InputSystem/Assets/01_Scripts/PlayerInput.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private UserInputAction inputAction;
+
+    public Action<Vector2> OnMovement;
+    public Action OnJump;
+
+    private bool uiMode = false;
+
+    private void Awake()
+    {
+        inputAction = new UserInputAction();
+        inputAction.Player.Enable();
+        inputAction.Player.Jump.performed += Jump;
+
+        inputAction.UI.Submit.performed += UIPerformPress;
+
+        inputAction.Player.Disable();
+
+        inputAction.Player.Jump.PerformInteractiveRebinding()
+            .WithControlsExcluding("Mouse")
+            .WithCancelingThrough("<keyborad>/escape")
+            .OnComplete(op =>
+            {
+                Debug.Log(op.selectedControl.name);
+                op.Dispose();
+                inputAction.Player.Enable();
+            })
+            .OnCancel(op =>
+            {   
+                Debug.Log("취소되었습니다");
+                op.Dispose();
+                inputAction.Player.Enable();
+            })
+            .Start();
+    }
+
+    private void UIPerformPress(InputAction.CallbackContext context)
+    {
+        Debug.Log("UI 상태");
+    }
+
+    private void Update()
+    {
+        Vector2 inputDir = inputAction.Player.Movement.ReadValue<Vector2>();
+        OnMovement?.Invoke(inputDir);
+
+        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            inputAction.Disable();
+            if(uiMode == false)
+            {
+                inputAction.UI.Enable();
+            }
+            else
+            {
+                inputAction.Player.Enable();
+            }
+            uiMode = !uiMode;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        OnJump?.Invoke();
+    }
+}
---
+++ InputSystem/Assets/01_Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float gravityMultiplier = 1f;
+    [SerializeField] private float jumpPower = 3f;
+
+    private CharacterController characterController;
+    public bool IsGround => characterController.isGrounded;
+
+    private Vector2 inputDirection;
+    private Vector3 movementVelocity;
+    public Vector3 MovmentVelocity;
+    private float verticalVelocity;
+
+    public bool ActiveMove { get; set; } = true;
+    
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.OnMovement += SetPlayerMovement;
+        playerInput.OnJump += Jump;
+    }
+
+    private void Jump()
+    {
+        if(!IsGround) return;
+        verticalVelocity += jumpPower;
+    }
+
+    private void SetPlayerMovement(Vector2 dir)
+    {
+        inputDirection = dir;
+    }
+
+    private void CalculateMovement()
+    {
+        movementVelocity = new Vector3(inputDirection.x, 0, inputDirection.y) * (moveSpeed * Time.fixedDeltaTime);
+
+        if(movementVelocity.sqrMagnitude > 0)
+        {
+            transform.rotation = Quaternion.LookRotation(movementVelocity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if(IsGround && verticalVelocity < 0)
+        {
+            verticalVelocity = -1f;
+        }
+        else
+        {
+            verticalVelocity += gravity * gravityMultiplier * Time.fixedDeltaTime;
+        }
+        movementVelocity.y = verticalVelocity;
+    }
+
+    private void Move()
+    {
+        characterController.Move(movementVelocity);
+    }
+
+    private void FixedUpdate()
+    {
+        if(ActiveMove)
+        {
+            CalculateMovement();
+        }
+        ApplyGravity();
+        Move();
+    }
+
+    public void StopImmediately()
+    {
+        movementVelocity = Vector3.zero;
+    }
+
+    public void SetMovement(Vector3 value)
+    {
+        verticalVelocity = value.y;
+        movementVelocity = new Vector3(value.x, 0, value.z);
+    }
+}
